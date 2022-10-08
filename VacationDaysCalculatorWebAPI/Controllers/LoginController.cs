5924569663a056af8493c0075a77147933d3ce8e
unknown <duje.miljevic@gmail.com>
2022-10-08 19:36:59 +0200

    added autentification and authorization on web API

---
+++ VacationDaysCalculatorWebAPI/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using DomainModel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.CodeDom.Compiler;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using VacationDaysCalculatorWebAPI.DatabaseContext;
+
+namespace VacationDaysCalculatorWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        private readonly VCDDbContext _VCDDbContext;
+
+        public LoginController(IConfiguration config, VCDDbContext vCDDbContext)
+        {
+            _config = config;
+            _VCDDbContext = vCDDbContext;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+            if(user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+            return NotFound("User not found");
+        }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(u => u.UserName == userLogin.UserName && u.Password == userLogin.Password);
+            if (currentUser != null)
+                return currentUser;
+            return null;
+        }
+    }
+}
