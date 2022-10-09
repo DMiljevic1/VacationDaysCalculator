572a43271883e1d4f062a515d8e239603ee3ad45
unknown <jnince01@gmail.com>
2022-10-09 10:02:49 +0200

    Blazor Server

---
+++ VacationDaysCalculatorBlazorServer/Models/EmployeeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VacationDaysCalculatorBlazorServer.Models
+{
+	public class EmployeeModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string CompanyPosition { get; set; }
+
+	}
+}
