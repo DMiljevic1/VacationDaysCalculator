94d8f4c640d2aad12154ce8f8274489dba04dc6f
Toni Jakovcevic <toni.jakovcevic10@gmail.com>
2022-10-16 15:19:05 +0200

    added login page without functionality

---
+++ VacationDaysCalculatorBlazorServer/Pages/RazorPageBases/LoginPageBase.cs
@@ -0,0 +1,40 @@
+﻿using DomainModel.Models;
+using Microsoft.AspNetCore.Components;
+using VacationDaysCalculatorBlazorServer.Service;
+using System;
+using System.Threading.Tasks;
+
+
+namespace VacationDaysCalculatorBlazorServer.Pages.RazorPageBases
+{
+    public class LoginPageBase : ComponentBase
+    {
+        public string username, password;
+
+        protected List<User> Users { get; set; }
+
+        [Inject]
+        protected UserService _userService { get; set; }
+
+        [Inject]
+        protected NavigationManager _navigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Users = await _userService.GetUsers();
+        }
+
+        protected async void AuthorizeLogin()
+        {
+            var user = await _userService.GetUserByUserName(username, password);
+            if (user == null)
+            {
+
+            }
+            else
+            {
+                //implementirat token nekako jos
+                _navigationManager.NavigateTo("/");
+            }
+    }
+}
