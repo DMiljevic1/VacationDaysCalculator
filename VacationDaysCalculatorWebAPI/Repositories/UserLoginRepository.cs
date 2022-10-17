2785a399f97393b1bd908ad1cc38da1db2d97dea
Toni Jakovcevic <toni.jakovcevic10@gmail.com>
2022-10-17 07:54:20 +0200

    addedRepositories

---
+++ VacationDaysCalculatorWebAPI/Repositories/UserLoginRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainModel.Models;
+using VacationDaysCalculatorWebAPI.DatabaseContext;
+
+namespace VacationDaysCalculatorWebAPI.Repositories
+{
+    public class UserLoginRepository
+    {
+        private readonly VCDDbContext _vCDDbContext;
+
+        public UserLoginRepository(VCDDbContext vCDDbContext)
+        {
+            _vCDDbContext = vCDDbContext;
+        }
+
+        public UserLogin getCurrentUser(UserLogin userLogin)
+        {
+
+        }
+    }
+}
---
+++ VacationDaysCalculatorWebAPI/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using DomainModel.Models;
+using VacationDaysCalculatorWebAPI.DatabaseContext;
+
+namespace VacationDaysCalculatorWebAPI.Repositories
+{
+    public class UserRepository
+    {
+        private readonly VCDDbContext _vCDDbContext;
+
+        public UserRepository(VCDDbContext vCDDbContext)
+        {
+            _vCDDbContext = vCDDbContext;
+        }
+
+        public List<User> getUsers()
+        {
+            return _vCDDbContext.Users.ToList();
+        }
+
+        public void insertUser(User user)
+        {
+            _vCDDbContext.Users.Add(user);
+            _vCDDbContext.SaveChanges();
+        }
+
+        public User getUser(int userId)
+        {
+            return _vCDDbContext.Users.FirstOrDefault(user => user.Id == userId);
+        }
+
+    }
+}
