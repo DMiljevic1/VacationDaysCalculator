1446d3a1c834b33815e9b1d5d9b664dc6d3a31db
unknown <duje.miljevic@gmail.com>
2022-12-17 17:02:21 +0100

    removed weather data and fixed bug with logout

--- VacationDaysCalculatorBlazorServer/Program.cs
+++ VacationDaysCalculatorBlazorServer/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
-using VacationDaysCalculatorBlazorServer.Data;
 using VacationDaysCalculatorBlazorServer.Service;
 using VacationDaysCalculatorBlazorServer.Services;
 
@@ -10,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<LogInService>();
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<AdminService>();
