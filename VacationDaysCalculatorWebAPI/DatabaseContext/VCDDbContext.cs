e9529853b36a4072bb578ad3724bbab70c4c23c9
unknown <duje.miljevic@gmail.com>
2022-12-17 15:21:34 +0100

    Deleted Weekend table

--- VacationDaysCalculatorWebAPI/DatabaseContext/VCDDbContext.cs
+++ VacationDaysCalculatorWebAPI/DatabaseContext/VCDDbContext.cs
@@ -10,7 +10,6 @@ namespace VacationDaysCalculatorWebAPI.DatabaseContext
         public DbSet<VacationDays> VacationDays { get; set; }
         public DbSet<Holiday> Holidays { get; set; }
         public DbSet<RemainingVacationDays> RemainingVacationDays { get; set; }
-        public DbSet<Weekend> Weekends { get; set; }
         public DbSet<User> Users { get; set; }
     }
 }
