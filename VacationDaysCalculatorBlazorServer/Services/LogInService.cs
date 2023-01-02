11e190cd24b066b2a68b0459241ba40d0522f4cf
unknown <duje.miljevic@gmail.com>
2023-01-02 21:22:46 +0100

    now you can login using enter and added error message if username or password is null

--- VacationDaysCalculatorBlazorServer/Services/LogInService.cs
+++ VacationDaysCalculatorBlazorServer/Services/LogInService.cs
@@ -22,25 +22,30 @@ namespace VacationDaysCalculatorBlazorServer.Services
 
         public async Task SendUserAsync(UserLogin userLogin)
         {
-            var httpPostRequest = new HttpRequestMessage(HttpMethod.Post, BaseApiUrl);
-            httpPostRequest.Content = new StringContent(JsonSerializer.Serialize(userLogin), Encoding.UTF8, "application/json");
-            var httpResponseMessage = await _httpClient.SendAsync(httpPostRequest);
-
-            var jwtToken = httpResponseMessage.Content.ReadAsStringAsync();
-            if(jwtToken.Result != "User not found")
-            {
-                await _customAuthenticationStateProvider.SetTokenAsync(jwtToken.Result);
-                var identity = await _customAuthenticationStateProvider.GetAuthenticationStateAsync();
-                var claims = identity.User.Identities.First().Claims.ToList();
-                if (claims[2].Value == "Employee")
-                    _navigationManager.NavigateTo("/Employee");
-                else
-                    _navigationManager.NavigateTo("/Admin");
-            }
+            if(userLogin.UserName == null || userLogin.Password == null)
+                LoginPageBase.message = "Username or password cannot be empty!";
             else
             {
-                LoginPageBase.message = "Incorrect username or password!";
-                _navigationManager.NavigateTo("LoginPage");
+                var httpPostRequest = new HttpRequestMessage(HttpMethod.Post, BaseApiUrl);
+                httpPostRequest.Content = new StringContent(JsonSerializer.Serialize(userLogin), Encoding.UTF8, "application/json");
+                var httpResponseMessage = await _httpClient.SendAsync(httpPostRequest);
+
+                var jwtToken = httpResponseMessage.Content.ReadAsStringAsync();
+                if(jwtToken.Result != "User not found")
+                {
+                    await _customAuthenticationStateProvider.SetTokenAsync(jwtToken.Result);
+                    var identity = await _customAuthenticationStateProvider.GetAuthenticationStateAsync();
+                    var claims = identity.User.Identities.First().Claims.ToList();
+                    if (claims[2].Value == "Employee")
+                        _navigationManager.NavigateTo("/Employee");
+                    else
+                        _navigationManager.NavigateTo("/Admin");
+                }
+                else
+                {
+                    LoginPageBase.message = "Incorrect username or password!";
+                    _navigationManager.NavigateTo("LoginPage");
+                }
             }
         }
     }
