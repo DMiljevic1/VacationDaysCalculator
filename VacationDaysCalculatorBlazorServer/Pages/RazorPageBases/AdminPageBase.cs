a113403c41da6d029131938fc506e0558f72fac7
unknown <duje.miljevic@gmail.com>
2023-01-13 14:50:35 +0100

    added admin table using mud blazor

--- VacationDaysCalculatorBlazorServer/Pages/RazorPageBases/AdminPageBase.cs
+++ VacationDaysCalculatorBlazorServer/Pages/RazorPageBases/AdminPageBase.cs
@@ -2,6 +2,7 @@
 using DomainModel.Enums;
 using DomainModel.Models;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using System;
 using VacationDaysCalculatorBlazorServer.Service;
 using VacationDaysCalculatorBlazorServer.Services;
@@ -10,6 +11,12 @@ namespace VacationDaysCalculatorBlazorServer.Pages.RazorPageBases
 {
     public class AdminPageBase : ComponentBase
     {
+        protected bool dense = false;
+        protected bool hover = true;
+        protected bool striped = false;
+        protected bool bordered = false;
+        protected string searchString1 = "";
+
         protected Vacation selectedVacation { get; set; }
         [Inject]
         protected AdminService _adminService { get; set; }
@@ -19,9 +26,26 @@ namespace VacationDaysCalculatorBlazorServer.Pages.RazorPageBases
         protected NavigationManager _navigationManager { get; set; }
         protected ConfirmationDialog CancelConfirmationDialog { get; set; }
         protected AdminDetails adminDetails { get; set; }
+
+        protected IEnumerable<Vacation> vacations = new List<Vacation>();
         protected override async Task OnInitializedAsync()
         {
             adminDetails = await _adminService.GetAdminDetailsAsync();
+            vacations = adminDetails.EmployeeVacationDays;
+        }
+        public bool FilterFunction(Vacation vacationRequests) => FilterFunc(vacationRequests, searchString1);
+
+        private bool FilterFunc(Vacation vacationRequests, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+                return true;
+            if (vacationRequests.User.LastName.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+            if (vacationRequests.User.FirstName.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+            if ($"{vacationRequests.Status}".Contains(searchString))
+                return true;
+            return false;
         }
         protected async Task ApproveVacationAsync(Vacation vacation)
         {
@@ -29,6 +53,7 @@ namespace VacationDaysCalculatorBlazorServer.Pages.RazorPageBases
             vacation.ApprovedBy = adminDetails.LastName + " " + adminDetails.FirstName;
             await _employeeService.ApproveVacationAsync(vacation);
             adminDetails = await _adminService.GetAdminDetailsAsync();
+            vacations = adminDetails.EmployeeVacationDays;
         }
         protected async Task CancelVacationAsync(Vacation vacation)
         {
@@ -50,6 +75,7 @@ namespace VacationDaysCalculatorBlazorServer.Pages.RazorPageBases
                 selectedVacation.Status = VacationStatus.Cancelled;
                 await _employeeService.CancelVacationAsync(selectedVacation);
                 adminDetails = await _adminService.GetAdminDetailsAsync();
+                vacations = adminDetails.EmployeeVacationDays;
             }
         }
     }
