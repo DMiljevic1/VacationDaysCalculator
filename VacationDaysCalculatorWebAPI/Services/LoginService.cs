d0904d1749e2039983e5fbd9ac38e9c04989b959
unknown <duje.miljevic@gmail.com>
2023-01-17 21:21:16 +0100

    added one way hash for passwords and exported logic for login from controller to services folder in web api

---
+++ VacationDaysCalculatorWebAPI/Services/LoginService.cs
@@ -0,0 +1,74 @@
+﻿using DomainModel.DtoModels;
+using DomainModel.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using VacationDaysCalculatorWebAPI.DatabaseContext;
+using VacationDaysCalculatorWebAPI.Repositories;
+
+namespace VacationDaysCalculatorWebAPI.Services
+{
+    public class LoginService
+    {
+        private IConfiguration _config;
+        private readonly VacationDbContext _VCDDbContext;
+        private readonly EmployeeRepository _userRepository;
+
+        public LoginService(IConfiguration config, VacationDbContext vCDDbContext, EmployeeRepository employeeRepository)
+        {
+            _config = config;
+            _VCDDbContext = vCDDbContext;
+            _userRepository = employeeRepository;
+        }
+        public string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Rsa, user.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public User Authenticate(UserLogin userLogin)
+        {
+            var users = _userRepository.GetUsers();
+            var currentUser = users.FirstOrDefault(u => u.UserName == userLogin.UserName && u.Password == HashUserPassword(userLogin.Password));
+            if (currentUser != null)
+                return currentUser;
+            return null;
+        }
+        private string HashUserPassword(string plainTextPassword)
+        {
+            string hashedPassword;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                hashedPassword = GetHash(sha256Hash, plainTextPassword);
+            }
+            return hashedPassword;
+        }
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
