0037ab397d4c9ee09f9930004b7adbf01d0747d3
unknown <duje.miljevic@gmail.com>
2023-01-21 16:55:38 +0100

    added change password page and added home and change password in nav bar

---
+++ VacationDaysCalculatorBlazorServer/Pages/RazorPageBases/ChangePasswordBase.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.DtoModels;
+using Microsoft.AspNetCore.Components;
+
+namespace VacationDaysCalculatorBlazorServer.Pages.RazorPageBases
+{
+	public class ChangePasswordBase : ComponentBase
+	{
+		public Password password { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            password = new Password();
+        }
+        protected async Task ChangePasswordAsync()
+        {
+
+        }
+    }
+}
