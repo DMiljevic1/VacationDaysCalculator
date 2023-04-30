ac6988de4f1483fa746acaecbe753ad243932795
unknown <duje.miljevic@gmail.com>
2023-04-30 13:22:23 +0200

    added enum SickLeaveStatus and made migration with new property

---
+++ VacationDaysCalculatorWebAPI/Migrations/20230430112114_addedEnumSickLeaveStatusInTableSickLeave.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VacationDaysCalculatorWebAPI.Migrations
+{
+    public partial class addedEnumSickLeaveStatusInTableSickLeave : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsClosed",
+                table: "SickLeave");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SickLeaveStatus",
+                table: "SickLeave",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SickLeaveStatus",
+                table: "SickLeave");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsClosed",
+                table: "SickLeave",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
