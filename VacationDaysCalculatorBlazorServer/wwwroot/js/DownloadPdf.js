9b749ebbf059d6683a58d510503b437eca1a8f4f
dmiljevic <duje.miljevic@nais.hr>
2023-08-04 13:28:20 +0200

    trying to fix download pdf from database

--- VacationDaysCalculatorBlazorServer/wwwroot/js/DownloadPdf.js
+++ VacationDaysCalculatorBlazorServer/wwwroot/js/DownloadPdf.js
@@ -1,8 +1,10 @@
-﻿function download(fileName, bytesBase64) {
+﻿function download(resultByte) {
+    var bytes = new Uint8Array(resultByte); // pass your byte response to this constructor
+
+    var blob = new Blob([bytes], { type: "application/pdf" });// change resultByte to bytes
+
     var link = document.createElement('a');
-    link.download = fileName;
-    link.href = 'data:application\pdf;base64, ' + bytesBase64;
-    document.body.appendChild(link);
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "myFileName.pdf";
     link.click();
-    document.body.removeChild(link);
-}  
+}
